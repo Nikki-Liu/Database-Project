5681efc4516966a78ba10bf9e712a4933f9c3bc7
vagrant <vagrant@homestead>
2020-04-29 00:21:35 +0000

    Basic Layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      Designed by <span style="color: #e27575;font-size: 14px;">Jingyi Liu</span> and <span style="color: #e27575;font-size: 14px;">Muwei Zheng</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:jl10018@nyu.edu">Contact us</a></p>
+  </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', 'Home Page')
+
+@section('content')
+  <h1>This is Home Page!</h1>
+@stop
